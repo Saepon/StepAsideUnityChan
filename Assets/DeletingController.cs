506a75e851c1e8fa7da116215f7aa9213c090192
Saepon <s.haisa.0426@gmail.com>
2020-06-29 18:31:10 +0900

    StepAsideUnityChan再提出
    宜しくお願いいたします

--- Assets/DeletingController.cs
+++ Assets/DeletingController.cs
@@ -4,18 +4,23 @@ using UnityEngine;
 
 public class DeletingController : MonoBehaviour
 {
+    private GameObject MainCamera;
+    private float difference;
     // Start is called before the first frame update
     void Start()
     {
+        this.MainCamera = GameObject.Find("Main Camera");
         
+        this.difference = this.transform.position.z -MainCamera.transform.position.z ;
+
     }
-       void OnBecameInvisible()
-        {
-         GameObject.Destroy(this.gameObject);
-         }
+     
     // Update is called once per frame
     void Update()
     {
- 
+   if (this. difference < -1)
+        {
+         GameObject.Destroy(this.gameObject);
+         }
     }
 }
